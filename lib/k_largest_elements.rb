9adc8db9702cd3c855e3441f7230dd3e2af7ff0b
srac1777 <shashank.racherla@gmail.com>
2018-09-06 10:46:09 -0700

    finish k largest elements

--- lib/k_largest_elements.rb
+++ lib/k_largest_elements.rb
@@ -1,5 +1,16 @@
 require_relative 'heap'
 
 def k_largest_elements(array, k)
-    
+    prc = Proc.new {|i,j| -1*(i <=> j)}
+    heap = BinaryMinHeap.new(&prc)
+    array.each do |el|
+        heap.push(el)
+    end
+    result = []
+    i = 0
+    while i < k
+        result << heap.extract
+        i += 1
+    end
+    result
 end
